782cc4582d11fdd4050848f4381cc627952b3a32
bwielk <bwielk@gmail.com>
2018-05-13 19:03:49 +0200

    Challenge two works

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -1,6 +1,11 @@
+import java.util.function.Function;
+
 public class Challenge1 {
 
     public static void main(String[] args) {
+
+
+        //CHALLENGE 1
 
 //        Runnable runnable = new Runnable(){
 //            @Override
@@ -21,7 +26,32 @@ public class Challenge1 {
             }
         };
 
+
+        //TESTS
         r.run();
+        System.out.println(everySecondChar.apply("ABCDEFGH"));
     }
+
+    //CHALLENGE 2
+
+//    public static String everySecondChar(String s){
+//        StringBuilder returnVal = new StringBuilder();
+//        for(int i=0; i<s.length(); i++){
+//            if(i%2 == 1){
+//                returnVal.append(s.charAt(i));
+//            }
+//        }
+//        return returnVal.toString();
+//    }
+
+    public static Function<String, String> everySecondChar = s -> {
+        StringBuilder returnVal = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+             if(i%2 == 1){
+                 returnVal.append(s.charAt(i));
+             }
+         }
+         return returnVal.toString();
+    };
 }
 
