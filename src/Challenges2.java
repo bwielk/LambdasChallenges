0687c5100d53454a532ca50aa0d645cd05f4261f
bwielk <bwielk@gmail.com>
2018-05-13 22:55:28 +0200

    Sorting list elements using streams works

--- src/Challenges2.java
+++ src/Challenges2.java
@@ -19,17 +19,25 @@ public class Challenges2 {
             "Jacob"
         );
 
-        List<String> namesCapitalized = new ArrayList<>();
+        List<String> namesCapitalized1 = new ArrayList<>();
         names.forEach(name -> {
             name = name.substring(0,1).toUpperCase() + name.substring(1);
-            namesCapitalized.add(name);
-            namesCapitalized.sort((n1, n2)->
-                n1.compareTo(n2));
+            namesCapitalized1.add(name);
+            //namesCapitalized.sort((n1, n2)-> n1.compareTo(n2));
+            namesCapitalized1.sort(String::compareTo);
         });
 
         //TESTS
-        System.out.println("/////////////////////////challenge6///////////////////////////////");
-        namesCapitalized.forEach(n -> System.out.println(n)); //returns the full list of sorted capitalizes names
+        System.out.println("/////////////////////////challenge6and7///////////////////////////////");
+        // returns the full list of sorted capitalizes names
+        //namesCapitalized.forEach(n -> System.out.println(n));
+        namesCapitalized1.forEach(System.out::println);
+        System.out.println("/////////////////////////challenge8///////////////////////////////");
+        List<String> namesCapitalized2 = new ArrayList<>();
+        names.stream()
+                .map(name -> name.substring(0,1).toUpperCase() + name.substring(1))
+                .sorted()
+                .forEach(System.out::println);
     }
 
 }
