2414460216945058ba5b068bdc686ce93d109765
bwielk <bwielk@gmail.com>
2018-05-19 17:47:46 +0200

    Investigating the use of the Stream reduce method

--- src/Challenges3.java
+++ src/Challenges3.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import javax.swing.text.DateFormatter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Challenges3 {
 
@@ -32,9 +33,21 @@ public class Challenges3 {
                 Comparator.comparing((String month) -> month.toUpperCase()));
         printList(months);
         System.out.println("////////////////////////MONTHS WITH J FIRST//////////////////////////");
+        String character = "M";
         months.sort(
-                Comparator.comparing((String month) -> month.toUpperCase().startsWith("J")).reversed());
+                Comparator.comparing((String month) -> month.toUpperCase().startsWith(character)).reversed());
         printList(months);
+
+        System.out.println("///////////////////////THE USE OF THE REDUCE METHOD//////////////////////////");
+        int sum = Stream.of(10, 20, 30)
+            .reduce(1, (accumulator, element) -> accumulator + element);
+        System.out.println(sum);
+        String conjoinedString = Stream.of("ABCD", "EFG", "HIJ", "KLMNO")
+                .reduce("", (acc, el) -> acc + el);
+        System.out.println(conjoinedString);
+        String firstAndSecondDigits = Stream.of("9812344", "76342212", "54223452", "3210002", "10993")
+                .reduce("", (acc, el) -> acc + el.substring(0,2));
+        System.out.println(firstAndSecondDigits);
     }
 
     private static void printList(List<String> months){
@@ -42,4 +55,6 @@ public class Challenges3 {
             System.out.println(m + " ==> " + m.length());
         }
     }
+
+
 }
